932fa35d834604e82f85bfe52bb68a39b7fa46a4
Max Hertel <maxherteljr@gmail.com>
2024-08-19 16:04:50 -0300

    user seeder

---
+++ laravel/database/seeders/UsuarioSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         // Excluir todos os usuários existentes
+         User::truncate();
+
+         // Criar o novo usuário
+         User::create([
+             'name' => 'Usuário Master',
+             'email' => 'rafaeldill16@gmail.com',
+             'password' => Hash::make('rafa@#234f3'), // Use Hash::make para criptografar a senha
+         ]);
+    }
+}
